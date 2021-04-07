a17ca4430421af6bc9881f4a852f867d169b54f0
bere abreo <berenicej2013@gmail.com>
2021-04-07 02:38:19 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -11,22 +11,18 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
-    thisIsCrazy();
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
-  sayMy();
-
   function sayMy() {
+    var name = "Kristin";    
     console.log(name);
-    var name = "Kristin";
   }
+  sayMy();
 }
